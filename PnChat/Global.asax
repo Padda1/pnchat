76755966e9797a5e9d38f1481d3c60aeaad60166
Padda1 <thepadda1@gmail.com>
2015-04-20 16:27:30 +0200

    init

---
+++ PnChat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PnChat.MvcApplication" Language="C#" %>
---
+++ PnChat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PnChat.Startup))]
+namespace PnChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
